94814fa11324a48a00d382caa796a8e4dc708697
davidramirezm30 <davidramirez.drm@gmail.com>
2019-03-18 12:51:57 +0100

    Add files via upload

--- oled_menu_opi.py
+++ oled_menu_opi.py
@@ -1,16 +1,24 @@
+#!/ust/bin/env python
+# -*- coding: utf-8 -*-
+
 from OPi import GPIO
-from PIL import ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDraw
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
 from time import sleep
 import socket
 import sys_info
+import logo
 import time
+import sys
+import os.path
+from demo_opts import get_device
 
-clk = 22
-dt = 21
-sw = 29
+
+clk = 15
+dt = 13
+sw = 11
 
 global gdraw, gdevice, menuindex, flag_menu
 flag_menu = False
@@ -21,12 +29,14 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-serial = i2c(port=0, address=0x3C)
+serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial, rotate=0)
 
 clkLastState = 0
 clkState = 0
+swState = 0
 
+## Shows the IP address
 def ip_address():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -37,6 +47,25 @@ def ip_address():
     except:
         return "IP no encontrada"
 	
+def logo(device):
+    img_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+        'images', 'decroly.png'))
+    logo = Image.open(img_path).convert("RGBA")
+    fff = Image.new(logo.mode, logo.size, (255,) * 4)
+
+    background = Image.new("RGBA", device.size, "white")
+    posn = ((device.width - logo.width) // 2, 0)
+    while True:
+        if (GPIO.event_detected(clk) == False or GPIO.event_detected(sw) == False):
+            sleep (10)
+            for angle in range(0, 360, 2):
+                rot = logo.rotate(angle, resample=Image.BILINEAR)
+                img = Image.composite(rot, fff, rot)
+                background.paste(img, posn)
+                device.display(background.convert(device.mode))
+        else:
+            menu_update()
+
 def invert(draw,x,y,text):
     font = ImageFont.load_default()
     draw.rectangle((x, y, x+120, y+10), outline=255, fill=255)
@@ -44,7 +73,7 @@ def invert(draw,x,y,text):
 	
 # Box and text rendered in portrait mode
 def menu(device, draw, menustr, index):
-    print "..........INIT............"
+    #print "..........INIT............"
     font = ImageFont.load_default()
     draw.rectangle(device.bounding_box, outline="white", fill="black")
     for i in range(len(menustr)):
@@ -52,52 +81,70 @@ def menu(device, draw, menustr, index):
             menuindex = i
             invert(draw, 2, i*10, menustr[i])
         else:
-		    draw.text((2, i*10), menustr[i], font=font, fill=255)
+            draw.text((2, i*10), menustr[i], font=font, fill=255)
 		
-		
-names = ['Memory', 'Disk', 'IPAddress', 'Network', 'CPUUsage', 'Settings']
+#names = ['Memoria virtual', 'Memoria interna', 'Direccion IP', 'Network', 'CPU Usage', 'Settings']
+names = ['Memoria virtual', 'Memoria interna', 'Direccion IP', 'Network', 'CPU Usage']
+
 with canvas(device) as draw:
-    menu(device, draw, names, 0)
+    menu(device, draw, names, 0)    
+    
+swState = GPIO.input(clk)
+    
+def back_to_menu(device, draw):
+    invert(draw, 0, 5, "Volver")
+    if swState == 1:
+        flag_menu = False
+        with canvas(device) as draw:
+            menu(device, draw, names, 0)
+
 
 def menu_operation(strval):
     global menuindex, flag_menu
-    if ( strval == "Memory"):
-        flag_menu = True
+    if ( strval == "Memoria virtual"):
+        #flag_menu = True
         with canvas(device) as draw:
-            draw.text((0, 26), sys_info.mem_usage(), fill="white")
-            #draw.text((30, 60), back(), fill="white")        
-    if ( strval == "Disk"):
-        flag_menu = True
+            draw.text((0, 26), sys_info.mem_usage(), fill="white")      
+            back_to_menu(device, draw)
+    if ( strval == "Memoria interna"):
+        #flag_menu = True
         with canvas(device) as draw:
             draw.text((0, 26), sys_info.disk_usage('/'), fill="white")
-            #draw.text((30, 60), back(), fill="white")        
+            #draw.text((0, 26), disk.render, fill="white")
+            back_to_menu(device, draw)       
     if ( strval == "Network"):
-        flag_menu = True
+        #flag_menu = True
         with canvas(device) as draw:
             draw.text((0, 26), sys_info.network('wlan0'), fill="white")
-    if ( strval == "CPUUsage"):
-        flag_menu = True
+            back_to_menu(device, draw)
+    if ( strval == "CPU Usage"):
+        #flag_menu = True
         with canvas(device) as draw:
             draw.text((0, 26), sys_info.cpu_usage(), fill="white")
-    if ( strval == "IPAddress"):
-        flag_menu = True
+            back_to_menu(device, draw)
+    if ( strval == "Direccion IP"):
+        #flag_menu = True
         with canvas(device) as draw:
             draw.text((0, 26), ip_address(), fill="white")
-    if ( strval == "Settings"):
-        flag_menu = True
-        with canvas(device) as draw:
+            back_to_menu(device, draw) 
+    #if ( strval == "Settings"):
+        #flag_menu = True
+        #logo(device)          
             #draw.rectangle(device.bounding_box, outline="white", fill="black")
-            draw.text((0, 26), "Spanish", fill="white")
-            draw.text((0, 52), "English", fill="white")       
+            #draw.text((0, 26), "Spanish", fill="white")
+            #draw.text((0, 52), "English", fill="white")     
 
 def sw_callback(channel):
     global flag_menu 
     global menuindex
     if flag_menu:
-        menu_update()
+        with canvas(device) as draw:
+            menu(device, draw, names, 0)
         flag_menu = True
-    strval = names[menuindex]
-    menu_operation(strval)
+    else:
+        strval = names[menuindex]
+        menu_operation(strval)
+        flase_menu = False
     
     
 clkLastState = GPIO.input(clk)
@@ -120,14 +167,16 @@ def rotary_callback(channel):
 def menu_update():
     global menuindex
     with canvas(device) as draw:
-        menu(device, draw, names, menuindex%6)       
-        
+        menu(device, draw, names, menuindex%len(names))
+
 try:
     GPIO.add_event_detect(sw, GPIO.FALLING , callback=sw_callback, bouncetime=100)
-    GPIO.add_event_detect(clk, GPIO.FALLING , callback=rotary_callback, bouncetime=100)  
-    raw_input("Enter anything") 
+    GPIO.add_event_detect(clk, GPIO.FALLING , callback=rotary_callback, bouncetime=100)
+
+    #logo(device)
+    input("Enter anything")
 except KeyboardInterrupt:  
-    print "Keyboard Interrupt"
+    print ("Keyboard Interrupt")
 	# here you put any code you want to run before the program   
 	# exits when you press CTRL+C  
     GPIO.cleanup()
@@ -135,10 +184,10 @@ except:
 	# this catches ALL other exceptions including errors.  
 	# You won't get any error messages for debugging  
 	# so only use it once your code is working  
-    print "Ending"  
+    print ("Ending") 
     GPIO.cleanup()
 finally:  
-    print "Finally"  
+    print ("Finally")
     GPIO.cleanup() # this ensures a clean exit		 
 		
 
